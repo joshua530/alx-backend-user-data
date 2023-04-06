e6d00e3c5239b5014156b3516080d1031daac22c
joshua530 <joshuaomari5@gmail.com>
2023-04-06 22:02:40 +0300

    format filtered_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,7 +8,7 @@ from os import environ
 import mysql.connector
 
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+PII_FIELDS = ("name", "email", "password", "phone", "ssn")
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -34,21 +34,41 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """returns database connector"""
+    """instantiates database connector"""
+    db = environ.get('PERSONAL_DATA_DB_NAME')
     username = environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
     password = environ.get('PERSONAL_DATA_DB_PASSWORD', '')
     host = environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = environ.get('PERSONAL_DATA_DB_NAME')
 
     db_log = mysql.connector.connection.MySQLConnection(user=username,
                                                         password=password,
                                                         host=host,
-                                                        database=db_name)
+                                                        database=db)
     return db_log
 
 
+class RedactingFormatter(logging.Formatter):
+    """ pii formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """instantiates formatter"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filters values in incoming log records using provided filters"""
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
+
+
 def main():
-    """main function"""
+    """everything runs from here"""
     db = get_db()
     cursor = db.cursor()
     cursor.execute("SELECT * FROM users;")
@@ -62,25 +82,6 @@ def main():
 
     cursor.close()
     db.close()
-
-
-class RedactingFormatter(logging.Formatter):
-    """ formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """filters values in incoming log records using filter_datum"""
-        record.msg = filter_datum(self.fields, self.REDACTION,
-                                  record.getMessage(), self.SEPARATOR)
-        return super(RedactingFormatter, self).format(record)
 
 
 if __name__ == '__main__':
