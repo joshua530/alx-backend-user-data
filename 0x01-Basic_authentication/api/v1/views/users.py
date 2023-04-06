fc2911ad8508bc312d0cdbe08b8723993cef033d
joshua530 <joshuaomari5@gmail.com>
2023-04-06 22:10:16 +0300

    arrange paths alphabetically

--- 0x01-Basic_authentication/api/v1/views/users.py
+++ 0x01-Basic_authentication/api/v1/views/users.py
@@ -16,41 +16,6 @@ def view_all_users() -> str:
     return jsonify(all_users)
 
 
-@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
-def view_one_user(user_id: str = None) -> str:
-    """ GET /api/v1/users/:id
-    Path parameter:
-      User ID
-    Return:
-      User object in json format
-      404 if the User ID doesn't exist
-    """
-    if user_id is None:
-        abort(404)
-    user = User.get(user_id)
-    if user is None:
-        abort(404)
-    return jsonify(user.to_json())
-
-
-@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
-def delete_user(user_id: str = None) -> str:
-    """ DELETE /api/v1/users/:id
-    Path parameter:
-      User ID
-    Return:
-      empty JSON object the User has been correctly deleted
-      404 if user with given id doesn't exist
-    """
-    if user_id is None:
-        abort(404)
-    user = User.get(user_id)
-    if user is None:
-        abort(404)
-    user.remove()
-    return jsonify({}), 200
-
-
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user() -> str:
     """ POST /api/v1/users/
@@ -87,6 +52,41 @@ def create_user() -> str:
         except Exception as e:
             error_msg = "Can't create User: {}".format(e)
     return jsonify({'error': error_msg}), 400
+
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def view_one_user(user_id: str = None) -> str:
+    """ GET /api/v1/users/:id
+    Path parameter:
+      User ID
+    Return:
+      User object in json format
+      404 if the User ID doesn't exist
+    """
+    if user_id is None:
+        abort(404)
+    user = User.get(user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_json())
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+def delete_user(user_id: str = None) -> str:
+    """ DELETE /api/v1/users/:id
+    Path parameter:
+      User ID
+    Return:
+      empty JSON object the User has been correctly deleted
+      404 if user with given id doesn't exist
+    """
+    if user_id is None:
+        abort(404)
+    user = User.get(user_id)
+    if user is None:
+        abort(404)
+    user.remove()
+    return jsonify({}), 200
 
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
